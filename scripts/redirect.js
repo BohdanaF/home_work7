3ac90c1efd109c2dbdde7f852d38e34c5cfc8c3d
bogdanafilimonenko <bfilimonenko@gmail.com>
2024-09-25 11:07:07 +0300

    layout, forms

---
+++ scripts/redirect.js
@@ -0,0 +1,10 @@
+const goToFormButton = document.querySelector("#redirect-btn");
+
+goToFormButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    window.location.href = '/home_work7/index.html';
+
+
+});
+
+
